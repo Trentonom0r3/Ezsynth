541d071f79ddf9fc0606b29139e6d6e7a06a098e
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-18 18:53:44 +0100

    Update imagesynthtest.py

--- examples/imagesynthtest.py
+++ examples/imagesynthtest.py
@@ -1,15 +1,14 @@
 from ezsynth import Imagesynth
 
+style = "style.png"  # 8 bit RGB
 
-STYLE =  "style.png" # 8 bit RGB
+src = "src.jpg"  # 8 bit RGB
+target = "tgt.jpg"  # 8 bit RGB
+weight = 0.5  # Weight
 
-SRC = "src.jpg" # 8 bit RGB
-TGT = "tgt.jpg" # 8 bit RGB
-WGT = 0.5 # Weight 
+output = "output.png"  # 8 bit RGB
 
-OUTPUT = "output.png" # 8 bit RGB
-
-synth = Imagesynth(STYLE)  # Create a new imagesynth object
-synth.add_guide(SRC, TGT, WGT)  # Add a new guide
-synth.run(OUTPUT)  # Run the synthesis and save.
+synth = Imagesynth(style)  # Create a new imagesynth object
+synth.add_guide(src, target, weight)  # Add a new guide
+synth.run(output)  # Run the synthesis and save.
 # result = synth.run()  # Run the synthesis and return the result as a numpy array
