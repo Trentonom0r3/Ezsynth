d4e52bda5aad9033eecfa1d320ad505b3cd520c9
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-27 22:12:10 +0100

    next

--- ezsynth/utils/guides/guides.py
+++ ezsynth/utils/guides/guides.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
-import torch
+
 from .edge_detection import EdgeDetector
 from ..flow_utils.OpticalFlow import OpticalFlowProcessor
 from ..flow_utils.warp import Warp
-#from utils import ebsynth, Preprocessor
+
+
+# from utils import ebsynth, Preprocessor
 
 class GuideFactory():
     """
@@ -55,10 +57,11 @@ class GuideFactory():
     VALID_EDGE_METHODS = ["PAGE", "PST", "Classic"]
     VALID_FLOW_METHODS = ["RAFT", "DeepFlow"]
     VALID_MODEL_NAMES = ["sintel", "kitti", "chairs"]
-    def __init__(self, imgsequence, imgseq, edge_method="PAGE", flow_method="RAFT", model_name="sintel"):
+
+    def __init__(self, imgsequence, imgseq, edge_method = "PAGE", flow_method = "RAFT", model_name = "sintel"):
         if not imgsequence:
             raise ValueError("Image sequence cannot be empty.")
-        
+
         self.imgsequence = imgsequence
         self.edge_method = edge_method if edge_method in self.VALID_EDGE_METHODS else "PAGE"
         self.flow_method = flow_method if flow_method in self.VALID_FLOW_METHODS else "RAFT"
@@ -66,24 +69,23 @@ class GuideFactory():
         self.guides = {}
         self.imgs = imgseq
 
-        
     def create_all_guides(self):
 
-        edge_guide = EdgeGuide(self.imgs, method=self.edge_method)
+        edge_guide = EdgeGuide(self.imgs, method = self.edge_method)
         edge_guide = edge_guide()
-        #edge_guide = [edge for edge in edge_guide]
-        flow_guide = FlowGuide(self.imgsequence, method=self.flow_method, model_name=self.model_name)
+        # edge_guide = [edge for edge in edge_guide]
+        flow_guide = FlowGuide(self.imgsequence, method = self.flow_method, model_name = self.model_name)
         flow_guide = flow_guide()
         flow_guide = [flow for flow in flow_guide]
-        #fwd_flow = FlowGuide(self.imgsequence[::-1], method=self.flow_method, model_name=self.model_name) # Reverse the image sequence
-        #fwd_flow = fwd_flow()   # Compute the flow, for some reason computing flow using imgseq backwards results in fwd_flow
-        #fwd_flow = [flow for flow in fwd_flow]
-        positional_guide = PositionalGuide(self.imgsequence, flow=flow_guide)
+        # fwd_flow = FlowGuide(self.imgsequence[::-1], method=self.flow_method, model_name=self.model_name) # Reverse the image sequence
+        # fwd_flow = fwd_flow()   # Compute the flow, for some reason computing flow using imgseq backwards results in fwd_flow
+        # fwd_flow = [flow for flow in fwd_flow]
+        positional_guide = PositionalGuide(self.imgsequence, flow = flow_guide)
         positional_guide = positional_guide()
-        positional_fwd = PositionalGuide(self.imgsequence, flow=flow_guide[::-1])
+        positional_fwd = PositionalGuide(self.imgsequence, flow = flow_guide[::-1])
         positional_fwd = positional_fwd()
         positional_fwd = positional_fwd[::-1]
-        fwd_flow = [flow*-1 for flow in flow_guide]
+        fwd_flow = [flow * -1 for flow in flow_guide]
 
         self.guides = {
             "edge": edge_guide,
@@ -92,15 +94,15 @@ class GuideFactory():
             "positional_rev": positional_guide,
             "positional_fwd": positional_fwd,
         }
-        
+
         return self.guides
-    
+
     def add_custom_guide(self, name, custom_guides):
         if len(custom_guides) != len(self.imgsequence):
             raise ValueError("The length of the custom guide must match the length of the image sequence.")
-        
+
         self.guides[name] = custom_guides
-        
+
     def __call__(self):
         return self.create_all_guides()
 
@@ -108,11 +110,12 @@ class GuideFactory():
 class Guide():
     def __init__(self):
         pass
-    
+
+
 class EdgeGuide(Guide):
     valid_methods = ["PAGE", "PST", "Classic"]
 
-    def __init__(self, imgseq, method="PAGE"):
+    def __init__(self, imgseq, method = "PAGE"):
         super().__init__()
         if method not in self.valid_methods:
             raise ValueError(f"Invalid method {method}. Valid methods are {self.valid_methods}")
@@ -142,24 +145,26 @@ class EdgeGuide(Guide):
     def _create(self, img):
 
         return self.edge_detector.compute_edge(img)
-            
+
+
 class FlowGuide(Guide):
     valid_methods = ["RAFT", "DeepFlow"]
+
     def __init__(self, imgseq, method = "RAFT", model_name = "sintel"):
         super().__init__()
         if method not in self.valid_methods:
             raise ValueError(f"Invalid method {method}. Valid methods are {self.valid_methods}")
-        self.optical_flow_processor = OpticalFlowProcessor(model_name= model_name, flow_method = method)
+        self.optical_flow_processor = OpticalFlowProcessor(model_name = model_name, flow_method = method)
         self.imgsequence = imgseq
         self.optical_flow = None
-        
+
     def __call__(self):
         return self._create()
-    
+
     def _create(self):
         self.optical_flow = self.optical_flow_processor.compute_flow(self.imgsequence)
         return self.optical_flow
-            
+
 
 class PositionalGuide(Guide):
     def __init__(self, imgseq, flow):
@@ -176,7 +181,7 @@ class PositionalGuide(Guide):
     def _create_and_warp_coord_map(self, flow_up, original_size):
         if self.coord_map is None:
             h, w = self.warp.H, self.warp.W
-            self.coord_map = np.zeros((h, w, 3), dtype=np.float32)
+            self.coord_map = np.zeros((h, w, 3), dtype = np.float32)
             self.coord_map[:, :, 0] = np.linspace(0, 1, w)
             self.coord_map[:, :, 1] = np.linspace(0, 1, h)[:, np.newaxis]
             self.coord_map_warped = self.coord_map.copy()
@@ -190,7 +195,6 @@ class PositionalGuide(Guide):
 
         # Update the original coord_map with the newly warped version for the next iteration
         self.coord_map = self.coord_map_warped.copy()
-
 
     def _create_g_pos_from_flow(self, flow_np, original_size):
         g_pos_files = []
@@ -207,7 +211,3 @@ class PositionalGuide(Guide):
     def _create(self):
         self.g_pos = self._create_g_pos_from_flow(self.flow, self.imgseq[0].shape[1::-1])
         return self.g_pos
-        
-
-    
-    
