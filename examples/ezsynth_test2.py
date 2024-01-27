87063724cffdb9c1c9c5b41d7b42b67a9a500942
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 00:14:58 +0100

    next

--- examples/ezsynth_test2.py
+++ examples/ezsynth_test2.py
@@ -4,23 +4,18 @@ import cv2
 
 import ezsynth.utils.ezutils
 
+config, guides, sequences = ezsynth.utils.ezutils.setup(
+    style_path = "styles",
+    input_path = "input",
+    edge_method = "Classic",
+    flow_method = "RAFT",
+    model_name = "sintel",
+)
 
-def main():
-    config, guides, sequences = ezsynth.utils.ezutils.setup(
-        style_path = "styles",
-        input_path = "input",
-        edge_method = "Classic",
-        flow_method = "RAFT",
-        model_name = "sintel",
-    )
+results = ezsynth.utils.ezutils.process(config, guides, sequences)
 
-    results = ezsynth.utils.ezutils.process(config, guides, sequences)
+output_folder = "output"
+os.makedirs(output_folder, exist_ok = True)
 
-    output_folder = "output"
-    os.makedirs(output_folder, exist_ok = True)
-
-    for i, image in enumerate(results):
-        cv2.imwrite(os.path.join(output_folder, "output" + str(i).zfill(3) + ".png"), image)
-
-
-main()
+for i, image in enumerate(results):
+    cv2.imwrite(os.path.join(output_folder, "output" + str(i).zfill(3) + ".png"), image)
