82ae3f0cf4a9e4f9b9cc540bd652c44da5836e12
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-27 19:36:33 +0100

    Update ezsynth_example.py

--- examples/ezsynth_example.py
+++ examples/ezsynth_example.py
@@ -1,15 +1,15 @@
 from ezsynth import Ezsynth
 
 a = Ezsynth(
-    styles=[
+    styles = [
         "styles/style000.jpg",
         "styles/style099.jpg",
     ],
-    imgsequence="input",
-    edge_method="Classic",
-    flow_method="RAFT",
-    model="sintel",
-    output_folder="output",
+    imgsequence = "input",
+    edge_method = "Classic",
+    flow_method = "RAFT",
+    model = "sintel",
+    output_folder = "output",
 )
 
 a.run()
