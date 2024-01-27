8421d36949c13c3e866f24739cf2448c5cc1e688
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-27 18:15:59 +0100

    Update imagesynth_test.py

--- examples/imagesynth_test.py
+++ examples/imagesynth_test.py
@@ -1,13 +1,13 @@
 from ezsynth import Imagesynth
 
-style = "../styles/style000.jpg"  # 8 bit RGB
-synth = Imagesynth(style)  # Create a new imagesynth object
+style = "../input/000.jpg"
+synth = Imagesynth(style)
 
-src = "../input/000.jpg"  # 8 bit RGB
-target = "../styles/style000.jpg"  # 8 bit RGB
-weight = 0.5  # Weight
-synth.add_guide(src, target, weight)  # Add a new guide
+src = "../input/000.jpg"
+target = "../styles/style000.jpg"
+weight = 0.5
+synth.add_guide(src, target, weight)
 
-output = "../output/000.jpg"  # 8 bit RGB
+output = "../output/000.jpg"
 synth.run(output)  # Run the synthesis and save.
 # result = synth.run()  # Run the synthesis and return the result as a numpy array
