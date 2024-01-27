118a185180e45f3990eb50754115bfba3c32e551
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 00:20:46 +0100

    next

--- examples/ezsynth_test2.py
+++ examples/ezsynth_test2.py
@@ -19,10 +19,10 @@ guides = create_guides(config)
 
 sequences = SequenceManager(config)._set_sequence()
 
-results = process(config, guides, sequences)
+images = process(config, guides, sequences)
 
 output_folder = "output"
 os.makedirs(output_folder, exist_ok = True)
 
-for i, image in enumerate(results):
+for i, image in enumerate(images):
     cv2.imwrite(os.path.join(output_folder, "output" + str(i).zfill(3) + ".png"), image)
