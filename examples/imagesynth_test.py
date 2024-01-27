ae70932b232b9583dbb1c2e2d0f4af0d044e0f74
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-27 19:14:30 +0100

    Update imagesynth_test.py

--- examples/imagesynth_test.py
+++ examples/imagesynth_test.py
@@ -1,13 +1,12 @@
 from ezsynth import Imagesynth
 
-style = "input/000.jpg"
-synth = Imagesynth(style)
+a = Imagesynth(style_img="input/000.jpg")
 
-src = "input/000.jpg"
-target = "styles/style000.jpg"
-weight = 0.5
-synth.add_guide(src, target, weight)
-
-output = "output/000.jpg"
-synth.run(output)  # Run the synthesis and save.
-# result = synth.run()  # Run the synthesis and return the result as a numpy array
+a.add_guide(
+    source="input/000.jpg",
+    target="styles/style000.jpg",
+    weight=0.5,
+)
+a.run(output_path="output/000.jpg")
+# or
+a.run()  # run and return numpy array
