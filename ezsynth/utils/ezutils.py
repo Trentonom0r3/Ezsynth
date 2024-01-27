2eca3fd2c6b293cca1b0e64294084a46a0fd87a9
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-27 22:30:48 +0100

    next

--- ezsynth/utils/ezutils.py
+++ ezsynth/utils/ezutils.py
@@ -17,7 +17,7 @@ from .sequences import SequenceManager
 def _get_image_paths(path: str) -> List[str]:
     """Get file names of image sequence from the directory."""
     if not os.path.isdir(path):
-        raise ValueError("img_sequence must be a valid directory.")
+        raise ValueError("Image path must be a valid directory.")
     a = sorted(os.listdir(path))
     return [
         os.path.join(path, filename) for filename in a
