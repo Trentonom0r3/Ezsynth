a77067e3b79b06fb6d0c0b999749e0feb42c4fe7
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-27 23:17:57 +0100

    next

--- ezsynth/utils/config.py
+++ ezsynth/utils/config.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Literal
 
 import numpy
 
@@ -8,6 +8,6 @@ import numpy
 class Config:
     styles: List[tuple[int, numpy.ndarray]]
     images: List[tuple[int, numpy.ndarray]]
-    edge_method: str
-    flow_method: str
-    model_name: str
+    edge_method: Literal["PAGE", "PST", "Classic"]
+    flow_method: Literal["RAFT", "DeepFlow"]
+    model_name: Literal["sintel", "kitti", "chairs"]
