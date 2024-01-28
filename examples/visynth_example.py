e21720ebebd238f1258750693e10e1f6147a67aa
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 17:45:51 +0100

    next

--- examples/visynth_example.py
+++ examples/visynth_example.py
@@ -11,14 +11,7 @@ config = Config(
     style_frames = image_sequence_from_directory("styles"),
 )
 
-guides = create_guides(config)
-
-sequences = SequenceManager(config)._set_sequence()
-
-images = process(config, guides, sequences)
-
-output_folder = "output"
-os.makedirs(output_folder, exist_ok = True)
+images = visynth(config)
 
 for i, image in enumerate(images):
     cv2.imwrite(os.path.join(output_folder, "output" + str(i).zfill(3) + ".png"), image)
