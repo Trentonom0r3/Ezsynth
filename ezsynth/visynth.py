930366fb820fd38eda4d72032e76a17938bdc5f1
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 23:19:43 +0100

    next

--- ezsynth/visynth.py
+++ ezsynth/visynth.py
@@ -57,7 +57,7 @@ def _process(a: Config, sequences: List[Sequence], guides: Guides):
                 return [x for x in images if x is not None]
 
             elif style_start is None and style_end is not None:
-                print("Running backward " + str(seq.start_frame) + "-" + str(seq.end_frame) + ".")
+                print("Running backward " + str(seq.start_frame) + "<-" + str(seq.end_frame) + ".")
                 images, _ = _run_sequences(a, guides, seq, (style_start, style_end), -1)
                 return [x for x in images if x is not None]
 
