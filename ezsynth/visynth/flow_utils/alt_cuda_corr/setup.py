3e70300b5a88b0113ecbaa0112c0d5bd2b59a3ee
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 18:00:59 +0100

    next

--- ezsynth/visynth/flow_utils/alt_cuda_corr/setup.py
+++ ezsynth/visynth/flow_utils/alt_cuda_corr/setup.py
@@ -1,33 +1,13 @@
-<<<<<<< Updated upstream
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-
 setup(
-    name='correlation',
-    ext_modules=[
+    name = 'correlation',
+    ext_modules = [
         CUDAExtension('alt_cuda_corr',
-            sources=['correlation.cpp', 'correlation_kernel.cu'],
-            extra_compile_args={'cxx': [], 'nvcc': ['-O3']}),
+                      sources = ['correlation.cpp', 'correlation_kernel.cu'],
+                      extra_compile_args = {'cxx': [], 'nvcc': ['-O3']}),
     ],
-    cmdclass={
+    cmdclass = {
         'build_ext': BuildExtension
     })
-
-=======
-from setuptools import setup
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
-
-
-setup(
-    name='correlation',
-    ext_modules=[
-        CUDAExtension('alt_cuda_corr',
-            sources=['correlation.cpp', 'correlation_kernel.cu'],
-            extra_compile_args={'cxx': [], 'nvcc': ['-O3']}),
-    ],
-    cmdclass={
-        'build_ext': BuildExtension
-    })
-
->>>>>>> Stashed changes
