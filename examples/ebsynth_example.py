e4322957a88a837c5d3a959be95940a2631b4f54
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 16:35:50 +0100

    next

--- examples/ebsynth_example.py
+++ examples/ebsynth_example.py
@@ -1,13 +1,7 @@
-from ezsynth import Imagesynth
+from ezsynth import *
 
-style = "input/000.jpg"
-synth = Imagesynth(style)
+e = Ebsynth()
 
-src = "input/000.jpg"
-target = "styles/style000.jpg"
-weight = 0.5
-synth.add_guide(src, target, weight)
+config = Config(style_image = "input/000.jpg", guides = [("input/000.jpg", "styles/style000.jpg", 0.5)])
 
-output = "output/000.jpg"
-synth.run(output)  # Run the synthesis and save.
-# result = synth.run()  # Run the synthesis and return the result as a numpy array
+e(config)
