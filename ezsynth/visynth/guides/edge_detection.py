792efb69d923c5041133b42d1e657c085ee288ea
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 18:58:45 +0100

    next

--- ezsynth/visynth/guides/edge_detection.py
+++ ezsynth/visynth/guides/edge_detection.py
@@ -60,7 +60,7 @@ class EdgeDetector:
 
         elif self.method == "PST":
             S, W, sigma_LPF, thresh_min, thresh_max, morph_flag = 0.3, 15, 0.15, 0.05, 0.9, True
-            result = self.pst_gpu.run(input_data_path, S, W, sigma_LPF, thresh_min, thresh_max, morph_flag)
+            result = self.pst_gpu.pst_output
             result = result.cpu().numpy()
             result = cv2.GaussianBlur(result, (5, 5), 3)
             result = (result * 255).astype(np.uint8)
