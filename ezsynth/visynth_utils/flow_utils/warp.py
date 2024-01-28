40ff7456f30d127461d5e48c6f562d71c74b87f6
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 20:15:27 +0100

    next

--- ezsynth/visynth_utils/flow_utils/warp.py
+++ ezsynth/visynth_utils/flow_utils/warp.py
@@ -15,12 +15,12 @@ class Warp:
         x, y = np.meshgrid(np.arange(width), np.arange(height))
         return np.stack((x, y), axis = -1).astype(np.float32)
 
-    def _warp(self, img, flo):
+    def _warp(self, image, flo):
         # with self.lock:
         flo_resized = cv2.resize(flo, (self.width, self.height), interpolation = cv2.INTER_LINEAR)
         map_x = self.grid[..., 0] + flo_resized[..., 0]
         map_y = self.grid[..., 1] + flo_resized[..., 1]
-        warped_img = cv2.remap(img, map_x, map_y, interpolation = cv2.INTER_LINEAR, borderMode = cv2.BORDER_REFLECT)
+        warped_img = cv2.remap(image, map_x, map_y, interpolation = cv2.INTER_LINEAR, borderMode = cv2.BORDER_REFLECT)
         return warped_img
 
     def run_warping(self, img, flow):
