3fe36fff499579b557c3a9607031e42fe95c5ef5
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 14:20:36 +0100

    next

--- examples/ezsynth_test.py
+++ examples/ezsynth_test.py
@@ -1,21 +1,28 @@
-from ezsynth import Ezsynth
+import os
 
-style_paths = [
-    "output000.jpg",
-    "output099.jpg",
-]
+import cv2
 
-image_folder = "input"
-output_folder = "output"
+from ezsynth.utils.config import config_from_directory
+from ezsynth.utils.ezutils import process
+from ezsynth.utils.guides.guides import create_guides
+from ezsynth.utils.sequences import SequenceManager
 
-ez = Ezsynth(
-    styles=style_paths,
-    imgsequence=image_folder,
-    edge_method="Classic",
-    flow_method="RAFT",
-    model="sintel",
-    output_folder=output_folder,
+config = config_from_directory(
+    style_path = "styles",
+    input_path = "input",
+    edge_method = "Classic",
+    flow_method = "RAFT",
+    model_name = "sintel",
 )
 
-ez.run()  # Run the stylization process
-results = ez.results  # The results are stored in the results variable
+guides = create_guides(config)
+
+sequences = SequenceManager(config)._set_sequence()
+
+images = process(config, guides, sequences)
+
+output_folder = "output"
+os.makedirs(output_folder, exist_ok = True)
+
+for i, image in enumerate(images):
+    cv2.imwrite(os.path.join(output_folder, "output" + str(i).zfill(3) + ".png"), image)
