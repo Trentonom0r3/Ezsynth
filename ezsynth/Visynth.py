ca383b3edeb764c5c4f333c59c659cd9b5ca524a
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 17:00:02 +0100

    next

--- ezsynth/Visynth.py
+++ ezsynth/Visynth.py
@@ -41,6 +41,30 @@ def config_from_directory(
     )
 
 
+def _get_image_paths(path: str) -> List[tuple[int, str]]:
+    try:
+        return sorted([
+            (_extract_index(x), os.path.join(path, x)) for x in os.listdir(path)
+        ])
+    except Exception:
+        raise ValueError("Cannot read images in: " + path)
+
+
+def _extract_index(name: str):
+    try:
+        pattern = re.compile(r"(\d+)\.(jpg|jpeg|png)$")
+        return int(pattern.findall(name)[0][0])
+    except Exception:
+        raise ValueError("Cannot extract index from: " + name)
+
+
+def _read_images(a: List[tuple[int, str]]) -> List[tuple[int, numpy.ndarray]]:
+    try:
+        return [(i, cv2.imread(b)) for i, b in a]
+    except Exception as e:
+        raise ValueError(f"Error reading image: {e}")
+
+
 def process(config: Config, guides: Guides, sequences: List[Sequence]):
     """
     Process sub-sequences using multiprocessing.
@@ -192,27 +216,3 @@ def run_sequences(imgseq, edge, flow,
 
         print(f"Final Length, Reverse = {reverse}: {len(stylized_frames)}. Error Length: {len(err_list)}")
         return stylized_frames, err_list
-
-
-def _get_image_paths(path: str) -> List[tuple[int, str]]:
-    try:
-        return sorted([
-            (_extract_index(x), os.path.join(path, x)) for x in os.listdir(path)
-        ])
-    except Exception:
-        raise ValueError("Cannot read images in: " + path)
-
-
-def _extract_index(name: str):
-    try:
-        pattern = re.compile(r"(\d+)\.(jpg|jpeg|png)$")
-        return int(pattern.findall(name)[0][0])
-    except Exception:
-        raise ValueError("Cannot extract index from: " + name)
-
-
-def _read_images(a: List[tuple[int, str]]) -> List[tuple[int, numpy.ndarray]]:
-    try:
-        return [(i, cv2.imread(b)) for i, b in a]
-    except Exception as e:
-        raise ValueError(f"Error reading image: {e}")
