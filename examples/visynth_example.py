913952e0c38063d028cc6709d570c82ee5fd0ca3
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 21:07:34 +0100

    next

--- examples/visynth_example.py
+++ examples/visynth_example.py
@@ -1,4 +1,5 @@
 import cv2
+import torch
 
 from ezsynth.visynth import Visynth, Config, image_sequence_from_directory
 
@@ -7,6 +8,8 @@ visynth = Visynth()
 config = Config(
     frames = image_sequence_from_directory("input"),
     style_frames = image_sequence_from_directory("styles"),
+    edge_method = "classic",
+    device = torch.device("cpu"),
 )
 
 images = visynth(config)
