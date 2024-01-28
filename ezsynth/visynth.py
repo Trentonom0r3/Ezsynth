f213b926ff274a5154d23993bc6d5a616ec9b01c
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-29 00:25:16 +0100

    next

--- ezsynth/visynth.py
+++ ezsynth/visynth.py
@@ -142,6 +142,8 @@ def _run_sequences(
         warp = Warp(a.frames[start_frame].image)
 
         for i in range(start_frame, end_frame, step):
+            print("Processing frame " + str(i + frame_offset))
+
             guides = [
                 (
                     guides.edge[start_frame],
