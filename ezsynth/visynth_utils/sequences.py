89dfa72b3b01bf91d83e8ef7a21325c86e145110
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 21:21:51 +0100

    next

--- ezsynth/visynth_utils/sequences.py
+++ ezsynth/visynth_utils/sequences.py
@@ -24,45 +24,44 @@ class Sequence:
         return f"Sequence: {self.begFrame} - {self.endFrame} | Style Start: {start_info} - Style End: {end_info}"
 
 
-class SequenceManager:
-    def config_to_sequences(self, begFrame, endFrame, styles, style_indexes, imgindexes):
-        """
-        Setup Sequence Information.
-        Compares the style indexes with the image indexes to determine the sequence information.
-        """
-        sequences = []
-        if self.num_styles == 1 and self.begFrame == self.style_indexes[0]:
-            sequences.append(
-                Sequence(begFrame = self.begFrame, endFrame = self.endFrame, style_start = cv2.imread(self.styles[0])))
-
-            return sequences
-
-        if self.style_indexes[0] == self.endFrame and self.num_styles == 1:
-            sequences.append(
-                Sequence(begFrame = self.begFrame, endFrame = self.endFrame, style_end = self.styles[0]))
-
-            return sequences
-
-        for i in range(self.num_styles - 1):
-
-            # If both style indexes are not None
-            if self.style_indexes[i] is not None and self.style_indexes[i + 1] is not None:
-                if self.style_indexes[i] == self.begFrame and self.style_indexes[i + 1] == self.endFrame:
-                    sequences.append(
-                        Sequence(self.begFrame, self.endFrame, self.styles[i], self.styles[i + 1]))
-
-                # If the first style index is the first frame in the sequence
-                elif self.style_indexes[i] == self.begFrame and self.style_indexes[i + 1] != self.endFrame:
-                    sequences.append(Sequence(
-                        self.begFrame, self.style_indexes[i + 1], self.styles[i], self.styles[i + 1]))
-
-                # If the second style index is the last frame in the sequence
-                elif self.style_indexes[i] != self.begFrame and self.style_indexes[i + 1] == self.endFrame:
-                    sequences.append(Sequence(
-                        self.style_indexes[i], self.endFrame, self.styles[i], self.styles[i + 1]))
-
-                elif self.style_indexes[i] != self.begFrame and self.style_indexes[i + 1] != self.endFrame and self.style_indexes[i] in self.imgindexes and self.style_indexes[i + 1] in self.imgindexes:
-                    sequences.append(Sequence(
-                        self.style_indexes[i], self.style_indexes[i + 1], self.styles[i], self.styles[i + 1]))
+def config_to_sequences(self, begFrame, endFrame, styles, style_indexes, imgindexes):
+    """
+    Setup Sequence Information.
+    Compares the style indexes with the image indexes to determine the sequence information.
+    """
+    sequences = []
+    if self.num_styles == 1 and self.begFrame == self.style_indexes[0]:
+        sequences.append(
+            Sequence(begFrame = self.begFrame, endFrame = self.endFrame, style_start = cv2.imread(self.styles[0])))
 
         return sequences
+
+    if self.style_indexes[0] == self.endFrame and self.num_styles == 1:
+        sequences.append(
+            Sequence(begFrame = self.begFrame, endFrame = self.endFrame, style_end = self.styles[0]))
+
+        return sequences
+
+    for i in range(self.num_styles - 1):
+
+        # If both style indexes are not None
+        if self.style_indexes[i] is not None and self.style_indexes[i + 1] is not None:
+            if self.style_indexes[i] == self.begFrame and self.style_indexes[i + 1] == self.endFrame:
+                sequences.append(
+                    Sequence(self.begFrame, self.endFrame, self.styles[i], self.styles[i + 1]))
+
+            # If the first style index is the first frame in the sequence
+            elif self.style_indexes[i] == self.begFrame and self.style_indexes[i + 1] != self.endFrame:
+                sequences.append(Sequence(
+                    self.begFrame, self.style_indexes[i + 1], self.styles[i], self.styles[i + 1]))
+
+            # If the second style index is the last frame in the sequence
+            elif self.style_indexes[i] != self.begFrame and self.style_indexes[i + 1] == self.endFrame:
+                sequences.append(Sequence(
+                    self.style_indexes[i], self.endFrame, self.styles[i], self.styles[i + 1]))
+
+            elif self.style_indexes[i] != self.begFrame and self.style_indexes[i + 1] != self.endFrame and self.style_indexes[i] in self.imgindexes and self.style_indexes[i + 1] in self.imgindexes:
+                sequences.append(Sequence(
+                    self.style_indexes[i], self.style_indexes[i + 1], self.styles[i], self.styles[i + 1]))
+
+    return sequences
