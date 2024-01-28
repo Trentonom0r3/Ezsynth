20475350a0cce067c558d4485d0b836ad2e87acc
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 23:28:31 +0100

    next

--- ezsynth/visynth_utils/sequences.py
+++ ezsynth/visynth_utils/sequences.py
@@ -15,11 +15,16 @@ def config_to_sequences(a: Config) -> List[Sequence]:
     Compares style frame indexes with image frame indexes to determine sequences.
     """
     acc = []
-    for b in a.style_frames:
-        start_frame = a.frames[0].index if len(acc) == 0 else acc[-1].end_frame
-        end_frame = b.index
+
+    def add(start_frame, end_frame):
         if start_frame < end_frame:
             acc.append(Sequence(start_frame = start_frame, end_frame = end_frame))
+
+    for b in a.style_frames:
+        add(
+            a.frames[0].index if len(acc) == 0 else acc[-1].end_frame,
+            b.index,
+        )
 
     if acc[-1].end_frame != a.frames[-1].index:
         acc.append(
