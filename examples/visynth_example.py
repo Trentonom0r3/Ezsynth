3c434784b1bfc666b782846d27b5b702baedd89e
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 17:45:47 +0100

    next

--- examples/visynth_example.py
+++ examples/visynth_example.py
@@ -4,6 +4,8 @@ import cv2
 
 from ezsynth.Visynth import Visynth, Config, image_sequence_from_directory
 
+visynth = Visynth()
+
 config = Config(
     frames = image_sequence_from_directory("input"),
     style_frames = image_sequence_from_directory("styles"),
