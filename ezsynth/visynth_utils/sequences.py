1d360dda0a29a4f03618421cac75883e0593a23b
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 21:47:23 +0100

    next

--- ezsynth/visynth_utils/sequences.py
+++ ezsynth/visynth_utils/sequences.py
@@ -21,12 +21,12 @@ def config_to_sequences(a: Config) -> List[Sequence]:
     """
     num_styles = len(a.style_frames)
 
-    if num_styles == 1 and a.frames[0][0] == style_indexes[0]:
+    if num_styles == 1 and a.frames[0][0] == a.style_frames[0][0]:
         return [
             Sequence(start_frame = (a.frames[0][0]), end_frame = (a.frames[-1][0]), style_start = cv2.imread(styles[0]))
         ]
 
-    if num_styles == 1 and a.frames[-1][0] == style_indexes[0]:
+    if num_styles == 1 and a.frames[-1][0] == a.style_frames[0][0]:
         return [
             Sequence(start_frame = (a.frames[0][0]), end_frame = (a.frames[-1][0]), style_end = styles[0])
         ]
@@ -35,23 +35,23 @@ def config_to_sequences(a: Config) -> List[Sequence]:
     for i in range(num_styles - 1):
 
         # If both style indexes are not None
-        if style_indexes[i] is not None and style_indexes[i + 1] is not None:
-            if style_indexes[i] == a.frames[0][0] and style_indexes[i + 1] == a.frames[-1][0]:
+        if a.style_frames[i][0] is not None and a.style_frames[i + 1][0] is not None:
+            if a.style_frames[i][0] == a.frames[0][0] and a.style_frames[i + 1][0] == a.frames[-1][0]:
                 sequences.append(
                     Sequence(a.frames[0][0], a.frames[-1][0], styles[i], styles[i + 1]))
 
             # If the first style index is the first frame in the sequence
-            elif style_indexes[i] == a.frames[0][0] and style_indexes[i + 1] != a.frames[-1][0]:
+            elif a.style_frames[i][0] == a.frames[0][0] and a.style_frames[i + 1][0] != a.frames[-1][0]:
                 sequences.append(Sequence(
-                    a.frames[0][0], style_indexes[i + 1], styles[i], styles[i + 1]))
+                    a.frames[0][0], a.style_frames[i + 1][0], styles[i], styles[i + 1]))
 
             # If the second style index is the last frame in the sequence
-            elif style_indexes[i] != a.frames[0][0] and style_indexes[i + 1] == a.frames[-1][0]:
+            elif a.style_frames[i][0] != a.frames[0][0] and a.style_frames[i + 1][0] == a.frames[-1][0]:
                 sequences.append(Sequence(
-                    style_indexes[i], a.frames[-1][0], styles[i], styles[i + 1]))
+                    a.style_frames[i][0], a.frames[-1][0], styles[i], styles[i + 1]))
 
-            elif style_indexes[i] != a.frames[0][0] and style_indexes[i + 1] != a.frames[-1][0] and style_indexes[i] in imgindexes and style_indexes[i + 1] in imgindexes:
+            elif a.style_frames[i][0] != a.frames[0][0] and a.style_frames[i + 1][0] != a.frames[-1][0] and a.style_frames[i][0] in imgindexes and a.style_frames[i + 1][0] in imgindexes:
                 sequences.append(Sequence(
-                    style_indexes[i], style_indexes[i + 1], styles[i], styles[i + 1]))
+                    a.style_frames[i][0], a.style_frames[i + 1][0], styles[i], styles[i + 1]))
 
     return sequences
