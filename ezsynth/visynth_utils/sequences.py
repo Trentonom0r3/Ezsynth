49cadc0b31e5e9d065d0248e0b00e4349e04a599
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-28 21:54:10 +0100

    next

--- ezsynth/visynth_utils/sequences.py
+++ ezsynth/visynth_utils/sequences.py
@@ -16,48 +16,15 @@ def config_to_sequences(a: Config) -> List[Sequence]:
     """
     Compares style frame indexes with image frame indexes to determine sequences.
     """
-    num_styles = len(a.style_frames)
-
-    if num_styles == 1 and a.frames[0][0] == a.style_frames[0][0]:
-        return [
+    acc = []
+    for b in a.style_frames:
+        acc.append(
             Sequence(
-                start_frame = a.frames[0][0],
-                end_frame = a.frames[-1][0],
-                style_start_frame = 0,
+                start_frame = 0,
+                end_frame = 0,
+                style_start_frame = None,
                 style_end_frame = None,
             )
-        ]
+        )
 
-    if num_styles == 1 and a.frames[-1][0] == a.style_frames[0][0]:
-        return [
-            Sequence(
-                start_frame = a.frames[0][0],
-                end_frame = a.frames[-1][0],
-                style_start_frame = None,
-                style_end_frame = 0,
-            )
-        ]
-
-    sequences = []
-    for i in range(num_styles - 1):
-
-        if a.style_frames[i][0] is not None and a.style_frames[i + 1][0] is not None:
-            if a.style_frames[i][0] == a.frames[0][0] and a.style_frames[i + 1][0] == a.frames[-1][0]:
-                sequences.append(
-                    Sequence(a.frames[0][0], a.frames[-1][0], styles[i], styles[i + 1]))
-
-            # If the first style index is the first frame in the sequence
-            elif a.style_frames[i][0] == a.frames[0][0] and a.style_frames[i + 1][0] != a.frames[-1][0]:
-                sequences.append(Sequence(
-                    a.frames[0][0], a.style_frames[i + 1][0], styles[i], styles[i + 1]))
-
-            # If the second style index is the last frame in the sequence
-            elif a.style_frames[i][0] != a.frames[0][0] and a.style_frames[i + 1][0] == a.frames[-1][0]:
-                sequences.append(Sequence(
-                    a.style_frames[i][0], a.frames[-1][0], styles[i], styles[i + 1]))
-
-            elif a.style_frames[i][0] != a.frames[0][0] and a.style_frames[i + 1][0] != a.frames[-1][0] and a.style_frames[i][0] in imgindexes and a.style_frames[i + 1][0] in imgindexes:
-                sequences.append(Sequence(
-                    a.style_frames[i][0], a.style_frames[i + 1][0], styles[i], styles[i + 1]))
-
-    return sequences
+    return acc
