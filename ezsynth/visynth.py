eb69ab8a15b8a3fe1f88e6013a1a7a4ae7e89b72
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-29 12:59:30 +0100

    next

--- ezsynth/visynth.py
+++ ezsynth/visynth.py
@@ -28,10 +28,10 @@ class Visynth:
 
         guides = config_to_guides(a)
 
-        return _process(a, guides, sequences)
+        return config_and_guides_and_sequences_to_ebsynth(a, guides, sequences)
 
 
-def _process(a: Config, guides: Guides, sequences: List[Sequence]) -> List[np.ndarray]:
+def config_and_guides_and_sequences_to_ebsynth(a: Config, guides: Guides, sequences: List[Sequence]) -> List[np.ndarray]:
     acc = []
     for seq in sequences:
         style_start = next((x[1] for x in a.style_frames if x[0] == seq.start_frame), None)
