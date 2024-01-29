be68d0b8b2227d990f2dcc6b11cd5c544bbd425e
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-29 12:30:06 +0100

    next

--- ezsynth/visynth_utils/config.py
+++ ezsynth/visynth_utils/config.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Dict
+from typing import List, Tuple
 from typing import Literal
 
 import numpy as np
@@ -20,14 +20,14 @@ class Config:
     """
     Visynth config.
     :param frames: List of video frames.
-    :param style_frames: Dictionary of frame indexes and style video frames.
+    :param style_frames: List of frame indexes and style video frames.
     :param edge_method: Method for edge detection. PAGE, PST or Classic. Default is PAGE.
     :param flow_method: Method for optical flow computation. RAFT or DeepFlow. Default is RAFT.
     :param flow_model: Model name for optical flow. sintel, kitti or chairs. Default is sintel.
     :param device: What processing unit to use.
     """
     frames: List[np.ndarray]
-    style_frames: Dict[int, np.ndarray]
+    style_frames: List[Tuple[int, np.ndarray]]
     edge_method: Literal["PAGE", "PST", "classic"] = "PAGE"
     flow_method: Literal["RAFT", "DeepFlow"] = "RAFT"
     flow_model: Literal["sintel", "kitti", "chairs"] = "sintel"
