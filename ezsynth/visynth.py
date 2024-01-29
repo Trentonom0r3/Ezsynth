75a7972401597c2af5edbc91f1ecae432ce40ba8
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-29 15:31:02 +0100

    next

--- ezsynth/visynth.py
+++ ezsynth/visynth.py
@@ -196,10 +196,7 @@ def _to_ebsynth_configs_helper_2pass(
                 )
             )
 
-            # Assuming frames[-1] is already in BGR format
-            frame = frames[-1] / 255.0
-
-            warped_img = warp.run_warping(frame, flow[i - 1] if direction == 1 else flow[i])
+            warped_img = warp.run_warping(frames[-1] / 255.0, flow[i - 1] if direction == 1 else flow[i])
             warped_img = cv2.resize(warped_img, frames[0].shape[1::-1])
 
             ebsynth_guides.append(
